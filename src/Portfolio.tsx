3007cccc0c8a9a72aeb54552fad6b96a8cd09ba5
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2017-12-29 10:57:20 -0800

    Get React working

---
+++ src/Portfolio.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class Portfolio extends React.PureComponent {
+    render() {
+        return (
+            <h1>Hi</h1>
+        );
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Portfolio from './Portfolio';
+import './index.scss';
+
+
+ReactDOM.render(
+  <Portfolio />,
+  document.getElementById('root')
+);
