a3960bb851d093662619a454eb6216a4c3916aaa
Adnan Chowdhury <adnanchowdhury88@gmail.com>
2017-12-31 12:44:51 -0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 yarn build
 cd dist
-g init
-g add -A
-g commit -m 'Deploy $(date +%s)'
-g remote add origin https://github.com/bttf/bttf.github.io
-g push origin master --force
+git init
+git add -A
+git commit -m 'Deploy $(date +%s)'
+git remote add origin https://github.com/bttf/bttf.github.io
+git push origin master --force
